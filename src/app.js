a7a11d071ec17917077655a3fcf31e7790c2f2a0
Nilton Vasques <nilton.vasques@openmailbox.org>
2016-09-28 01:13:07 -0300

    Add some css

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { TodoList } from './components';
 
 const dummyTodos = [
   { id: 0, isDone: true,  text: 'make components' },
-  { id: 1, isDone: false, text: 'design actions' },
+  { id: 1, isDone: true, text: 'design actions' },
   { id: 2, isDone: false, text: 'implement reducer' },
   { id: 3, isDone: false, text: 'connect components' }
 ];
